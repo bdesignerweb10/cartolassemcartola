fc22abca39bd3502af2805be7a7c534bf04d6018
BRUNO GOMES DA SILVA <bdesigner_web@hotmail.com>
2018-03-23 15:24:05 -0300

    correção capas

--- src/mata-mata.php
+++ src/mata-mata.php
@@ -3,21 +3,19 @@ require_once('header.php');
 ?>
 <main id="mainmata">
 	<div id="mata-mata" class="container">
+		<div class="row capa-matamata"></div>
 	</div><!-- container -->
 </main>
 <main id="mainconfrontos">	
 	<div class="container">
-		<div class="row capa-matamata">
-				
-		</div>	
 		<div class="row nome-mata">
 			<div class="col-sm-6">
 				<h2 class="headline" id="nome-mata-mata"></h2>
 			</div>	
 			<div class="col-sm-6">
 				<div class="btn btn-info vol-mata" id="voltar-mata-mata"><a href="#"><i class='fa fa-arrow-left'></i>&nbsp;&nbsp;&nbsp;Voltar para a lista de mata-mata</div>
-			</div>
-		</div>
+			</div>	
+		</div>	
 		<div class="row">
 			<div class="col-12">
 				<ul class="nav nav-pills nav-fill nav-tabs" id="nav-niveis">
